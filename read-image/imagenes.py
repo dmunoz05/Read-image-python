47bee9e95dd1958c0401980542d7fe6701845309
= <daniel20025febrero29@gmail.com>
2024-02-18 14:19:46 -0500

    Other form for scale gray read img

--- read-image/imagenes.py
+++ read-image/imagenes.py
@@ -5,6 +5,9 @@ import numpy as np
 img = cv2.imread("girasol.jpg")
 img = cv2.resize(img,(300,250))
 
+imgGray0 = cv2.imread("girasol.jpg", 0)
+imgGray0 = cv2.resize(imgGray0, (300, 250))
+
 #Kernel
 kernel = np.ones((5, 5), np.uint8)
 
@@ -16,7 +19,8 @@ imgDilation = cv2.dilate(imgCanny, kernel, iterations=1)
 imgEroded = cv2.erode(imgDilation, kernel, iterations=1)
 
 cv2.imshow("Imagen Original", img)
-cv2.imshow("Imagen Gris", imgGray)
+cv2.imshow("Imagen Gris 1", imgGray)
+cv2.imshow("Imagen Gris 2", imgGray0)
 cv2.imshow("Imagen Blur", imgBlur)
 cv2.imshow("Imagen Canny", imgCanny)
 cv2.imshow("Imagen Dilatacion", imgDilation)
